0e16fe6ab57c5684f0e058263d9d0978e1a3120d
Milad Makram <milad.makram@db.com>
2022-03-10 12:42:42 +0000

    Initial commit for this project

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -0,0 +1,40 @@
+package io.javabrains.springbootstarter.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    // @RequestMapping(method = RequestMethod.POST)
+    @PostMapping("/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+    @PutMapping("/topics/{id}")
+    public void updateTopic(@PathVariable String id, @RequestBody Topic topic) {
+        topicService.updateTopic(id, topic);
+    }
+
+    @DeleteMapping("/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
+    }
+
+}
